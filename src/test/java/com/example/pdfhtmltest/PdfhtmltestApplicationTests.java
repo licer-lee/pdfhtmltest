d7ee5d360795c823203246ceedf6063e9e3724cb
liwenchao <liwenchao_consultant@mbcloud.com>
2020-06-04 20:49:27 +0800

    增加base64

--- src/test/java/com/example/pdfhtmltest/PdfhtmltestApplicationTests.java
+++ src/test/java/com/example/pdfhtmltest/PdfhtmltestApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.pdfhtmltest;
 
+import org.apache.commons.codec.binary.Base64;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.FileCopyUtils;
+
+import java.io.*;
 
 @SpringBootTest
 class PdfhtmltestApplicationTests {
@@ -11,6 +15,14 @@ class PdfhtmltestApplicationTests {
     }
 
     @Test
-    public void testBase64(){
+    public void testBase64() throws IOException {
+
+        //https://blog.csdn.net/weixin_39800144/article/details/80225990
+        File f = new File("d://beanLifeCircle.png");
+        byte[] b = FileCopyUtils.copyToByteArray(f);
+        Base64 base64=new Base64();
+        b=base64.encode(b);
+        String s=new String(b);
+        System.out.println("s = " + s);
     }
 }
